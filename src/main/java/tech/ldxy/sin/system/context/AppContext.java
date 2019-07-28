2f708b1174104571e90f9cde22c5231a3d0fa0a4
Castle <1455936807@qq.com>
2019-07-28 08:11:30 +0800

    归类测试文件

--- src/main/java/tech/ldxy/sin/system/context/AppContext.java
+++ src/main/java/tech/ldxy/sin/system/context/AppContext.java
@@ -1,5 +1,6 @@
 package tech.ldxy.sin.system.context;
 
+import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -52,5 +53,60 @@ public class AppContext implements ApplicationContextAware {
      */
     public static ApplicationContext getApplicationContext() {
         return applicationContext;
+    }
+
+    /**
+     * 根据名字获取 Spring 容器中注册的 Bean 实例
+     * 如果找不到该 Bean 的实例, 则抛出 org.springframework.beans.factory.NoSuchBeanDefinitionException
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(String name){
+        return (T) applicationContext.getBean(name);
+    }
+
+    /**
+     * 根据类型获取 Spring 容器中注册的 Bean 实例
+     * 如果找不到该 Bean 的实例, 则抛出 org.springframework.beans.factory.NoSuchBeanDefinitionException
+     */
+    public static <T> T getBean(Class<T> clz){
+        return applicationContext.getBean(clz);
+    }
+
+    /**
+     * 根据名字查询 Spring 容器中是否存在某个 Bean 实例
+     */
+    public static boolean containsBean(String name) {
+        return applicationContext.containsBean(name);
+    }
+
+    /**
+     * 判断以给定名字注册的 Bean 是 singleton 还是 prototype。
+     * 如果与给定名字相应的 Bean 定义没有被找到，将抛出 org.springframework.beans.factory.NoSuchBeanDefinitionException
+     */
+    public static boolean isSingleton(String name){
+        return applicationContext.isSingleton(name);
+    }
+
+    /**
+     * 根据 Spring 中注册的 Bean 的名称, 获取其类型
+     * 如果找不到 Bean 的实例, 则抛出 org.springframework.beans.factory.NoSuchBeanDefinitionException
+     */
+    public static Class<?> getType(String name){
+        return applicationContext.getType(name);
+    }
+
+    /**
+     * 根据 Spring 中注册的 Bean 的名称, 获取其别名
+     */
+    public static String[] getAliases(String name){
+        return applicationContext.getAliases(name);
+    }
+
+    /**
+     * 获取 AOP 代理对象
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getAopProxy(T invoker){
+        return (T) AopContext.currentProxy();
     }
 }
